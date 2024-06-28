1fb15e352e768ca3b3e016aa72ebfb959eac29a7
DanielePilati <165797370+DanielePilati@users.noreply.github.com>
2024-06-28 14:59:04 +0200

    removed arraylist

--- src/main/java/org/ciclabile/java/IntegerList.java
+++ src/main/java/org/ciclabile/java/IntegerList.java
@@ -1,36 +1,33 @@
 package org.ciclabile.java;
 
-import java.util.ArrayList;
-
 public class IntegerList {
 
 	
-	private ArrayList<Integer> integerList = new ArrayList<Integer>();
+	private int[] intArray;
 	private int counter = 0;
 	
 	public IntegerList(int[] integerArray){
-		for(int number : integerArray) {
-			this.integerList.add(number);
-		}
 		//La classe deve avere un costruttore che prende come parametro un array di interi (che sarà l’elenco che viene gestito internamente)
+		this.intArray = integerArray;
+	
 	}
 	
 	public int getElementoSuccessivo() {
 		// (che restituisce il prossimo elemento rispetto all’ultima volta che è stato invocato. Cioè la prima volta restituisce il primo elemento, la seconda volta il secondo, …)
 		counter ++;
-		return this.integerList.get(counter-1);
+		return this.intArray[counter-1];
 	}
 	public boolean hasAncoraElementi() {
 		// (che deve restituire true se ci sono ancora elementi da restituire)
 		boolean result = true;
-		if (this.counter >= this.integerList.size()) {
+		if (this.counter >= this.intArray.length) {
 			result = false;
 		}
 		return result;
 	}
 
-	public ArrayList<Integer> getList() {
-		return this.integerList;
+	public int[] getList() {
+		return this.intArray;
 	}
 	
 	
@@ -45,6 +42,7 @@ public class IntegerList {
 			System.out.println(intList.getElementoSuccessivo());
 			System.out.println("ci sonno altri elementi da scorrere? = "+intList.hasAncoraElementi());
 		}
+		
 		
 	}
 	
